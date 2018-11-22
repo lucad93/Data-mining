69f569e618fb68c351afaf35f9f25a49f657d9ff
Alessio Bollea <alessio.bollea@live.it>
2018-11-22 10:03:10 +0100

    Laboratorio 2, giorno 1

---
+++ Laboratorio 2/Es1 - 22 Novembre/main1.m
@@ -0,0 +1,83 @@
+clear
+close all
+clc
+
+%% Multi-Class classification
+nc = 30;                                % number of samples per class
+c = 7;                                  % number of classes
+r = 5;                                  % radius
+X = [];
+Y = [];
+
+% We create the training set: a circle of radius r
+for i = 1:c
+    theta = i * (2 * pi / c);
+    X = [X; randn(nc, 1) + r*cos(theta), randn(nc, 1) + r*sin(theta)];
+    Y = [Y; i*ones(nc, 1)];
+end
+[n, d] = size(X);                       % total number of samples and dimension
+
+%Normalization
+for i = 1:d
+   mi = min(X(:,i));
+   ma = max(X(:,i));
+   diff = ma - mi;
+   X(:,i) = 2 * (X(:,i) - mi) / diff - 1;
+end
+
+% Plotting
+colors = 'rbcmbyg';
+figure, box on, grid on, hold on
+for i = 1:c
+   plot(X(Y==i,1), X(Y==i,2), ['o', colors(i)]); 
+end
+
+%% Implementation of all vs all learning phase
+lambda = 1;                             % regularization term
+W = [];
+% All the classes must compete against all the others, so two nested loops
+for i1 = 1:c
+   for i2 = i1+1:c
+       cp = Y == i1;                    % class plus
+       cm = Y == i2;                    % class minus
+       all = cp | cm;                   % all the points, labelled as +1 or -1
+       Ytmp = cp*1 + cm*(-1);           
+       W = [W, (X(all,:)'*X(all,:) + lambda*eye(d))\(X(all,:)'*Ytmp(all))]; % i store all Ws
+   end
+end
+
+%% Classification phase
+ns = 10000;
+XS = 2 * rand(ns, d) - 1;
+YS = zeros(ns, c * (c-1) / 2);
+i3 = 0;                                 % tells which model i'm using in a particular iteration
+for i1 = 1:c
+   for i2 = i1+1:c
+      i3 = i3 + 1;
+      YS(:, i3) = XS * W(:, i3);
+      % I go back to the original class (1,..,7)
+      YS(:, i3) = (YS(:, i3) >= 0)*i1 + (YS(:, i3) < 0)*i2;
+   end
+end
+YS = mode(YS, 2);                       % i take the most frequent results, row-wise
+
+% Plot
+for i = 1:c
+   plot(XS(YS==i,1), XS(YS==i,2), ['.', colors(i)], 'MarkerSize', 1); 
+end
+
+% In the model there is no bias, so all serparators pass through the origin
+% If we reduce nc, the classes will not have the same area anymore
+% lambda should be chosen not a priori, but reapiting the process for
+% different values and selecting the one with the lowest error
+
+
+
+
+
+
+
+
+
+
+
---
+++ Laboratorio 2/Es1 - 22 Novembre/main2.m
@@ -0,0 +1,57 @@
+clear
+close all
+clc
+
+%% Support Vector Machines
+n = 100;
+d = 2;
+X = [randn(n/2, d) + 2; randn(n/2, d) - 2];
+Y = [ones(n/2, 1); -ones(n/2, 1)];
+
+figure, hold on, box on, grid on
+plot(X(Y<0, 1), X(Y<0, 2), 'ob');
+plot(X(Y>0, 1), X(Y>0, 2), 'or');
+
+%% Learning phase
+% dual formulation: min_alpha .5 * alpha' Q alpha - 1 alpha
+% s.t. y'alpha = 0, 0 <= alpha <= C, C has the opposite meaning of lambda
+% Q_ij = y_i y_j x'_i x_j
+% We use quadprog to solve this problem, which solves quadratic programming
+% problems, even if the matlab algorithm doesn't work very well for SVMs
+C = 1;
+Q = diag(Y) * (X * X') * diag(Y);             % waste of memory but it's faster than two nested loops
+alpha = quadprog(Q, -ones(n, 1), [], [], Y', 0, zeros(n, 1), C*ones(n, 1));
+w = X' * diag(Y) * alpha;
+% The bias is computed only for alpha in (0, C)
+i = find(alpha > 0+1e-5 | alpha < C-(+1e-5)); i = i(1);
+b = Y(i) - w' * X(i, :)';
+
+%Normalization
+for i = 1:d
+   mi = min(X(:,i));
+   ma = max(X(:,i));
+   diff = ma - mi;
+   X(:,i) = 2 * (X(:,i) - mi) / diff - 1;
+end
+
+%% Classification phase
+ns = 10000;
+XS = 2 * rand(ns, d) - 1;
+YS = XS * w + b;
+
+% Plot
+plot(XS(YS<0, 1), XS(YS<0, 2), '.c', 'MarkerSize', 1);
+plot(XS(YS>0, 1), XS(YS>0, 2), '.m', 'MarkerSize', 1);
+plot(XS(YS<-1, 1), XS(YS<-1, 2), '.b', 'MarkerSize', 1);
+plot(XS(YS>+1, 1), XS(YS>+1, 2), '.r', 'MarkerSize', 1);
+
+% I plot the correct points and the ones that lay on the support hyperplanes
+plot(X(alpha == 0, 1), X(alpha == 0, 2), 'sg');
+plot(X(alpha > 0 & alpha < C, 1), X(alpha > 0 & alpha < C, 2), 'sk');
+plot(X(alpha == C, 1), X(alpha == C, 2), 'sy');
+
+% Primal and dual costs
+dualcost = -(.5 * alpha' * Q * alpha - ones(n, 1)' * alpha);
+primalcost = .5 * w' * w + C * sum(max(0, 1-diag(Y)*(X*w + b)));
+dualitygap = abs(dualcost - primalcost);
+title(sprintf('Duality Gap: %e', dualitygap));
