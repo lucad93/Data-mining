b640f9efdc56928ecc524b8318262e9e0282619e
Alessio Bollea <alessio.bollea@live.it>
2018-11-25 16:14:06 +0100

    Prime modifiche funzionanti, non completo

--- Laboratorio 2/Es1 - 22 Novembre/main1_best_lambda.m
+++ Laboratorio 2/Es1 - 22 Novembre/main1_best_lambda.m
@@ -3,8 +3,8 @@ close all
 clc
 
 %% Multi-Class classification
-nc = 30;                                % number of samples per class
-c = 7;                                  % number of classes
+nc = 80;                                % number of samples per class
+c = 4;                                  % number of classes
 r = 5;                                  % radius
 X = [];
 Y = [];
@@ -25,51 +25,57 @@ for i = 1:d
    X(:,i) = 2 * (X(:,i) - mi) / diff - 1;
 end
 
-% Plotting
-colors = 'rbcmkyg';
+%% Implementation of all vs all learning phase
+nl = round(.7 * n);                     % learning set dimension = 70% of total data
+nv = n - nl;                            % validation set dimension = the remaining data
+
+err = zeros(30, 1);                     % error vector (every value is for a different value of lambda)
+for k = 1:30
+    i = randperm(n);                    % vector of random data indexes
+    il = sort(i(1:nl));                 % vector of learning data indexes
+    iv = sort(i(nl+1:end));             % vector of validation data indexes
+    j = 0;                              % index for the error vector
+    
+    % Definition of learning and validation sets
+    XL = X(il,:);
+    YL = Y(il);
+    XV = X(iv,:);
+    YV = Y(iv);
+    
+    for lambda = logspace(-4,3,30)          % regularization term
+        W = [];
+        % All the classes must compete against all the others, so two nested loops
+        for i1 = 1:c
+           for i2 = i1+1:c
+               cp = YL == i1;                % class plus
+               cm = YL == i2;                % class minus
+               all = cp | cm;               % all the points, labelled as +1 or -1
+               Ytmp = cp*1 + cm*(-1);           
+               W = [W, (XL(all,:)'*XL(all,:) + lambda*eye(d))\(XL(all,:)'*Ytmp(all))];
+           end
+        end
+
+        % Classification phase
+        YS = zeros(nv, c * (c-1) / 2);          % vector of predicted labels
+        i3 = 0;                                 % tells which model i'm using in a particular iteration
+        for i1 = 1:c
+           for i2 = i1+1:c
+              i3 = i3 + 1;
+              YS(:, i3) = XV * W(:, i3);
+              % I go back to the original class (1,..,7)
+              YS(:, i3) = (YS(:, i3) >= 0)*i1 + (YS(:, i3) < 0)*i2;
+           end
+        end
+        YS = mode(YS, 2);                       % i take the most frequent results, row-wise
+    end
+end
+
+% Plotting the learning set
+colors = 'bygkcmr';
 figure, box on, grid on, hold on
 for i = 1:c
-   plot(X(Y==i,1), X(Y==i,2), ['o', colors(i)]); 
+   plot(XL(YL==i,1), XL(YL==i,2), ['o', colors(i)]); 
 end
-
-%% Implementation of all vs all learning phase
-lambda = 1;                             % regularization term
-W = [];
-% All the classes must compete against all the others, so two nested loops
-for i1 = 1:c
-   for i2 = i1+1:c
-       cp = Y == i1;                    % class plus
-       cm = Y == i2;                    % class minus
-       all = cp | cm;                   % all the points, labelled as +1 or -1
-       Ytmp = cp*1 + cm*(-1);           
-       W = [W, (X(all,:)'*X(all,:) + lambda*eye(d))\(X(all,:)'*Ytmp(all))]; % i store all Ws
-   end
-end
-
-%% Classification phase
-ns = 10000;
-XS = 2 * rand(ns, d) - 1;
-YS = zeros(ns, c * (c-1) / 2);
-i3 = 0;                                 % tells which model i'm using in a particular iteration
-for i1 = 1:c
-   for i2 = i1+1:c
-      i3 = i3 + 1;
-      YS(:, i3) = XS * W(:, i3);
-      % I go back to the original class (1,..,7)
-      YS(:, i3) = (YS(:, i3) >= 0)*i1 + (YS(:, i3) < 0)*i2;
-   end
-end
-YS = mode(YS, 2);                       % i take the most frequent results, row-wise
-
-% Plot
-for i = 1:c
-   plot(XS(YS==i,1), XS(YS==i,2), ['.', colors(i)], 'MarkerSize', 1); 
-end
-
-% In the model there is no bias, so all serparators pass through the origin
-% If we reduce nc, the classes will not have the same area anymore
-% lambda should be chosen not a priori, but reapiting the process for
-% different values and selecting the one with the lowest error
 
 
 
